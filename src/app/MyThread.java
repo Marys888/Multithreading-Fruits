46f626304b85142468e6246daaf696fdb20e91cb
Marys888 <severovamaha1999@gmail.com>
2025-05-20 12:11:20 +0200

    Add MyThread class logic to run DataHandler in separate threads

--- src/app/MyThread.java
+++ src/app/MyThread.java
@@ -1,4 +1,13 @@
 package app;
 
-public class MyThread {
+public class MyThread extends Thread {
+    DataHandler dataHandler;
+    public MyThread(String name, DataHandler dataHandler){
+        super(name);
+        this.dataHandler = dataHandler;
+    }
+    @Override
+    public void run() {
+        dataHandler.getOutput();
+    }
 }
