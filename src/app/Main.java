a0e9a9dc096781d1290a3e6fdc85dfda3138e8b8
Marys888 <severovamaha1999@gmail.com>
2025-05-20 12:15:44 +0200

    Update Application entry point to start threads using DataHandler

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,11 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
+        DataHandler dh = new DataHandler();
+        MyThread myThread1 = new MyThread("Thread1", dh);
+        MyThread myThread2 = new MyThread("Thread2", dh);
 
+        myThread1.start();
+        myThread2.start();
     }
 }
